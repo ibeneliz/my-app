42a69bbb1ba22aff656629ae18f64623ae03eb13
ibeneliz <ibeneliz@gmail.com>
2024-01-21 01:36:45 +0530

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Gallery from './gallery.js';
+import PackingList from './packingList';
 
 function App() {
   return (
     <div className="App">
       <Gallery />
+      <PackingList />
     </div>
   );
 }
